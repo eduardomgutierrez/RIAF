7b4f68cfb1ee5d450d95ca5c9d0e77bb28ad0ab1
Eduardo M. Gutiérrez <emgutierrez@iar.unlp.edu.ar>
2020-07-25 16:59:48 -0300

    Add files via upload

--- global_initCond.py
+++ global_initCond.py
@@ -7,6 +7,7 @@ G = const.gravitational_constant * 1.0e3
 cLight = const.speed_of_light * 1.0e2
 cLight2 = cLight*cLight
 solarMass = 1.98847e33
+eddAccRate = 1.39e18
 
 def virialTemp(r):
     return 3.6e12/r
@@ -14,32 +15,44 @@ def virialTemp(r):
 adafFile = 'adafFile.txt'
 adafParameters = 'adafParameters.txt'
 
+# Fix parameters
+coronaFactor = 1.0
+SSDdisk = 1
+pIndex = 1.0
+correctorAccRate = 0.01
+
 # INITIAL PARAMETERS
 
-blackHoleMass = 4.0e6   # [Msol]
-beta = 0.9
-alpha = 0.1
-delta = 0.5
+blackHoleMass = 1.0e8       # [Msol]
+eddAccRate = eddAccRate * blackHoleMass
+accRateNorm = 1e-1          # [MdotEdd]
+rOut = 1e2                  # [Rs]
+accRateOut = accRateNorm * eddAccRate * coronaFactor
+accRateCD = accRateNorm * eddAccRate
+innerRadiusSSD = 2.0        # [Rs]
 
-rOut = 1.0e4
-eddAccRate = 1.39e18 * blackHoleMass
-accRateNorm = 1.0e-5
-accRateOut = accRateNorm*eddAccRate
+# Adimensional Disk parameters
+beta = 0.9
+alpha = 0.3
+delta = 0.1
 s = 0.3
 
 # EIGENVALUES
+log10j0 = np.log10(0.8373)
+log10j1 = np.log10(2.61)
 
-log10j0 = np.log10(1.4)
-log10j1 = np.log10(1.43)
-
-# INITIAL VALUES
+# Boundary conditions
 
 # rOut ~ 10^2 rS
 #temp_i_Out = 0.6*virialTemp(rOut)
 #temp_e_Out = 0.08*virialTemp(rOut)
 #lamda = 0.5
 
-# rOut ~ 10^4 rS
-temp_i_Out = 0.2*virialTemp(rOut)
-temp_e_Out = 0.19*virialTemp(rOut)
-lamda = 0.2
+if np.abs(np.log10(rOut/1.0e2)) < np.abs(np.log10(rOut/1.0e4)):
+    temp_i_Out = 0.6*virialTemp(rOut)
+    temp_e_Out = 0.08*virialTemp(rOut)
+    lamda = 0.5
+else:
+    temp_i_Out = 0.2*virialTemp(rOut)
+    temp_e_Out = 0.19*virialTemp(rOut)
+    lamda = 0.2
